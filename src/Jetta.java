30af0c009eff0f0e198db62d6a9eda4bbc6f4ca5
KaiqueDev1 <kakamelo141205@gmail.com>
2026-01-03 17:20:49 -0300

    Classe comum com tratamento dos métodos da Classe Abstrata e interface. Utilização de Polimorfismo, herança, atributos e métodos

--- src/Jetta.java
+++ src/Jetta.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+public class Jetta extends Carro implements CoisasDeCarro{
+    public boolean acelera = true;
 
-public class Jetta {
+    @Override
+    public void Acelerar() {
+        acelera = acelera;
+        System.out.println("O Jetta  " + (acelera ? "está acelerando" : "não esta acelerando"));
+    }
+
+    // método publico a ser chamado
+    public void CambioE(){
+        alternarCambio();
+        Cambio(); // chama método da interface
+    }
+
+    // método da interface
+    @Override
+    public void Cambio() {
+
+        System.out.println("Cambio " + (automatico ? "automático" : "manual"));
+    }
 }
